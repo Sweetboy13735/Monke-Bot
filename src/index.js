b1562d437e6c034318bb087d127b8bab8058ed54
Ramone Graham <ramonegraham@gmail.com>
2022-05-09 19:02:30 +0100

    Create core file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * Represents the core of the bot.
+ * When the program starts executing, this file is responsible for handling the bot's main functions.
+ * @author Ramone Graham
+ */
+// Initialise dotenv
+require("dotenv").config();
+
+// Package imports
+const { Client, Intents } = require("discord.js");
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Event callbacks
+client.once("ready", () => {
+	console.log("Ready!");
+});
+
+// Login to Discord using the bot token
+client.login(process.env.TOKEN);
